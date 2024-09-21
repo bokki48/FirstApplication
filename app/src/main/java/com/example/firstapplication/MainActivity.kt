e1ff9b2f67f4aba4b857250e37e9858f4a07ab3b
Chiril <BokKi48@yandex.ru>
2024-09-21 21:32:11 +0300

    correct commit

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.firstapplication
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var inputText: TextView
@@ -16,27 +15,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        inputText = findViewById(R.id.textViewInput)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        inputText = findViewById(R.id.editText)
         outputText = findViewById(R.id.textViewOutput)
         outputCount = findViewById(R.id.textViewOutputCount)
         button = findViewById(R.id.button)
 
-        onButtonClick(button)
-
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        button.setOnClickListener {
+            val sendText = inputText.text
+            outputText.text = sendText.reversed()
+            outputCount.text = countSymbol(sendText.toString()).toString()
         }
     }
 
-    fun onButtonClick(view: View) {
-        val sendText = inputText.text
-        outputText.text = sendText.reversed()
-        outputCount.text = sendText.count().toString()
+    fun countSymbol(string: String): Int {
+        var count = 0
+        for (str in string) {
+            if (str == ' ') continue
+            count++
+        }
+        return count
     }
 }
